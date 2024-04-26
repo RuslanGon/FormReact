05aba8980072b4f1237023d3149383ab705852fd
RuslanGon <ruslan441006@gmail.com>
2024-04-26 09:52:31 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 
-
 import './App.css'
 
 import MailBox from './component/MailBox/MailBox'
 import MeestExpressUser from './component/MailBox/meestExpress.json'
 
 
-
-
-
 function App() {
  
-
-
-
   return (
    <div>
     <MailBox boxTitle="Meest Express"  boxUsers={MeestExpressUser} />
