f896ce93be77fff9624fbe3951ac7cdfc75a18bc
RuslanGon <ruslan441006@gmail.com>
2024-04-26 10:40:12 +0300

    wer

--- src/component/MailBoxForm/MailBoxForm.jsx
+++ src/component/MailBoxForm/MailBoxForm.jsx
@@ -1,21 +1,32 @@
 
 const MailBoxForm = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const userEmail = event.currentTarget.elements.userEmail.value
+        const userName = event.currentTarget.elements.userName.value
+        console.log(userEmail);
+        console.log(userName);
+    }
+
   return (
-   <form>
+   <form onSubmit={handleSubmit}>
     <h2>Add new users</h2>
     <label>
         <span>User email</span>
         <br />
         <input type="email" name="userEmail" placeholder="@email" required />
     </label>
-    <br />
-    <button type="submit"> ▶ Create new users</button>
+    
     <br />
     <label>
         <span>User name</span>
         <br />
         <input type="text" name="userName" placeholder="name" required />
     </label>
+    <br />
+    <br />
+    <button type="submit"> ▶ Create new users</button>
    </form>
   )
 }
