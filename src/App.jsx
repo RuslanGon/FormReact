0f65a5bd92868c932ce721c97c1fdb4e78fc48c9
RuslanGon <ruslan441006@gmail.com>
2024-04-26 10:07:15 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 
+import { useState } from 'react'
 import './App.css'
 
 import MailBox from './component/MailBox/MailBox'
 import MeestExpressUser from './component/MailBox/meestExpress.json'
+import { nanoid } from 'nanoid'
 
 
 function App() {
+
+  const [users, setUsers] = useState(MeestExpressUser)
+
+  const onAddUsers = (formData) => {
+    const finalUsers = {
+      ...formData,
+      id: nanoid()
+    }
+    setUsers([...users, finalUsers])
+  }
+
  
   return (
    <div>
-    <MailBox boxTitle="Meest Express"  boxUsers={MeestExpressUser} />
+    <MailBox boxTitle="Meest Express"  boxUsers={users} />
     
    </div>
   )
